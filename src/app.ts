4b9b06460732d3d955d38efabb19f3f2ce05a5e7
Jean Haro <JeanCHL98@hotmail.com>
2025-01-23 14:36:29 -0500

    75 - Tarea: Impresión de consola y archivo

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,38 @@
-const message: string = 'Hola Mundo';
+import fs from 'fs';
+import path from 'path';
 
-console.log(message);
+const number = 5;
+let bodyMessage: string = '';
+
+// \n -> salto de línea
+for (let i = 1; i <= 10; i++) {
+    bodyMessage += `${number} x ${i} = ${number * i}\n`;
+}
+
+const data = `
+==========================
+    Tabla del ${number}
+==========================
+
+${bodyMessage}
+`
+
+console.log(data);
+
+// Genera la carpeta
+const outputDir = path.join(__dirname, '../outputs');
+
+try {
+    // Verificamos si la carpeta existe
+    if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir); // Crea la carpeta si no oexiste
+
+    // Donde estará alojado el archivo
+    const filePath = path.join(outputDir, `tabla-${number}.txt`);
+
+    // Crea el archivo y coloca el contenido
+    fs.writeFileSync(filePath, data);
+
+    console.log('Archivo creado exitosamente');
+} catch (error) {
+    console.log('Error al escribir el archivo', error)
+}
