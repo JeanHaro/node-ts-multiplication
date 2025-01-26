38aaa94fd6615666964a749af2e8239b8912eccd
Jean Haro <JeanCHL98@hotmail.com>
2025-01-26 04:37:05 -0500

    86 - Tarea: Reforzar todo lo aprendido

--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -5,20 +5,25 @@ interface RunOptions {
     base: number;
     limit: number;
     showTable: boolean;
+    name?: string;
+    destination?: string;
 }
 
 // El que va a orquestar de como va a funcionar nuestra aplicación de entrada
 export class ServerApp {
     // Cuando nosotros lanzamos la aplicación queremos crear la tabla 
-    static run ({base, limit, showTable}: RunOptions) {
+    static run ({base, limit, showTable, name, destination}: RunOptions) {
         console.log('Server running...');
+
+        // Si el nombre viene igual entonces ...
+        if (name === "table") name = `table-${base}`;
 
         // Nuestro caso de uso
         const table = new CreateTable().execute({base, limit}); 
         const wasCreated = new SaveFile().execute({ 
             fileContent: table, 
-            fileDir: 'outputs',
-            fileName: `table-${base}`
+            fileDir: destination,
+            fileName: name
         });
         
         if (showTable) console.log(table);
